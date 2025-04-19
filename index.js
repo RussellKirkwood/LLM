4f52dffdf209141402d1dbcc65b8553e49b7c882
Russell Kirkwood <creatinggenius@gmail.com>
2025-04-19 10:05:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@
 //import * as genai from '@mediapipe/tasks-genai';
 //import {FilesetResolver, LlmInference} from 'https://russellkirkwood.github.io/LLM/node_modules/@mediapipe/tasks-genai/';
 import * as genai from 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-genai';
+import {FilesetResolver, LlmInference} from 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-genai/';
 
 const input = document.getElementById('input');
 const output = document.getElementById('output');
